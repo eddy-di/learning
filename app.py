4afca1b0d2124f93ee51805ed7f8fa19e4531006
Eddy <eddy.di.minur@gmail.com>
2022-11-26 21:29:22 +0600

    initial commit first steps

---
+++ app.py
@@ -0,0 +1,50 @@
+import math
+import datetime
+print("Hello World")
+print("*"*10)
+print("hello World")
+2 + 4
+x = 1
+y = 2
+unit_price = 4
+print(x + y)
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+course = "      Python \nProgramming  "
+print(course)
+first = "Eldiiar"
+last = "Mirlanov"
+full_name = f"{first} {last}"
+print(full_name)
+today = datetime.datetime.today()
+print(f"{today:%d %B, %Y}")
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.lstrip())
+print(course.rstrip())
+print(course.find("gram"))
+print(course.replace("m", "s"))
+print("gram" in course)
+print("gras" not in course)
+print(10+3)
+print(10+3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10 % 3)
+print(10**3)
+print(round(5.5))
+print(abs(-5.5))
+
+
+print(math.ceil(-7.7))
+print(math.comb(7, 5))
+print(math.copysign(7, 5))
+
+x = input("x: ")
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
