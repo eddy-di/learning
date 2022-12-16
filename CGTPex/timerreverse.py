f5a4e6dc582566cb41b08ea847aa8892b988e250
Eddy <eddy.di.minur@gmail.com>
2022-12-16 23:51:02 +0600

    asked to add mitues

--- CGTPex/timerreverse.py
+++ CGTPex/timerreverse.py
@@ -3,11 +3,17 @@ import time
 # Get the number of seconds from the user
 seconds = int(input("Enter the number of seconds for the timer: "))
 
-# Countdown from the number of seconds
-while seconds > 0:
-    print(seconds)
-    seconds -= 1
+# Calculate the number of minutes and seconds
+minutes, seconds = divmod(seconds, 60)
+
+# Countdown from the number of minutes and seconds
+while minutes > 0 or seconds > 0:
+    print(f"{minutes:02}:{seconds:02}")
     time.sleep(1)
+    seconds -= 1
+    if seconds < 0:
+        minutes -= 1
+        seconds = 59
 
 # Timer has reached zero
 print("Time's up!")
