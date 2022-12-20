6125f3cd89e2ae44490f277a3315af94844b39fa
Eddy <eddy.di.minur@gmail.com>
2022-12-20 11:36:39 +0600

    keyword argument example

---
+++ functions/keywordargument.py
@@ -0,0 +1,6 @@
+def increment (number, by):
+    return number + by
+
+
+print(increment(2, by=1)) #by=1 is a keyword argument when lots of arguments 
+#are used, to read and understand code better and faster
