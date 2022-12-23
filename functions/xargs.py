b9200b3ed0b4c4f260c96434242b64be8a9b2c62
Eddy <eddy.di.minur@gmail.com>
2022-12-23 13:30:55 +0600

    debigging test

--- functions/xargs.py
+++ functions/xargs.py
@@ -8,12 +8,12 @@ multiply(2, 3)  # we cannot add more variables to function multiply
 # Second case is use of *args where lots of variables can be inserted
 
 
-def m_multiply(*numbers): #*numbers is *args example and shows the 
-    #possibilty to include lots of variables for m_multiply
-    total = 1 #results are stored here
-    for number in numbers: #results in total are iterated with all available numbers
+def m_multiply(*numbers):  # *numbers is *args example and shows the
+    # possibilty to include lots of variables for m_multiply
+    total = 1  # results are stored here
+    for number in numbers:  # results in total are iterated with all available numbers
         total *= number
     return total
 
 
-print (m_multiply (3, 4, 5, 6, 7))
+print(m_multiply(3, 4, 5, 6, 7))
