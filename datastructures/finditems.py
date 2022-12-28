10e2ed20aab1d15606011f6e2bfa8132b17910b3
Eddy <eddy.di.minur@gmail.com>
2022-12-28 16:21:57 +0600

    count and if added

--- datastructures/finditems.py
+++ datastructures/finditems.py
@@ -1,2 +1,5 @@
-letters = ["a", "b", "c", "2"]
-print(letters.index("2"))
+letters = ["a", "b", "c", "2", "4", "e", "5", "8"]
+print(letters.count("3")) # count gives number of occurences of an item in a list
+if "3" in letters:
+    print(letters.index("3")) # without if the result will give an error of not having such item in list
+    
