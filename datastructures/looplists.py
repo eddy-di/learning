f0578de630123c3b887519e4a5b3d53d75e3bfb3
Eddy <eddy.di.minur@gmail.com>
2022-12-28 15:11:38 +0600

    enumerate in lists

--- datastructures/looplists.py
+++ datastructures/looplists.py
@@ -1,4 +1,5 @@
 letters = ["a", "b", "c", "d", "e"]
 # strandart for is used to iterate in loops
-for letter in letters:
-    print (letter)
+for index, letter in enumerate(letters): #if enumerate is added the end results is showing the index of iteration and it becomes a tuple
+#in this case the end result prints both the number of index and letter from the list    
+    print (index, letter) 
