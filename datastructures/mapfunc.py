0f4a49f388f9209f623ea4a75ac28c23af586f8a
Eddy <eddy.di.minur@gmail.com>
2023-01-02 13:46:00 +0600

    mapping with lambda

--- datastructures/mapfunc.py
+++ datastructures/mapfunc.py
@@ -4,9 +4,12 @@ items = [
     ("Product3", 12),
 ]
 
-prices = []
-for item in items:
-    prices.append(item[1])
+# prices = []
+# for item in items:
+    # prices.append(item[1])
 # in lines 7-9 there is function that help to map only 
 # integers from tuples in original list
+prices = list(map(lambda item: item[1], items))
+# this line of code is similar to the code in lines 7-9 but it 
+# uses lambda function and list built in func in Python
 print(prices)
