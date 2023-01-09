836389c3b6a38a909b30fc6d7b250eac03dbea9e
Eddy <eddy.di.minur@gmail.com>
2023-01-09 14:35:36 +0600

    added comments

--- exceptions/exception.py
+++ exceptions/exception.py
@@ -1,8 +1,10 @@
 # example when user enters not correct input
 try:
     age = int(input("Age: "))
-except ValueError:
+except ValueError as ex: # the ex part is considered as very technical, and may not be included, this is for illustation purpouses only
     print("You did not enter a valid age.")
-else:
+    print(ex)
+    print(type(ex))
+else: # this part of code is executed if the except part was not executed
     print("No exceptions were encountered.")
-print("Going on smoothly")
+print("Going on smoothly") # this message shows that the code is still operating and have not crashed
