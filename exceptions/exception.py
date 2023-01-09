211493e62a7ca5bca9545bebcd0331efd7d67652
Eddy <eddy.di.minur@gmail.com>
2023-01-09 16:03:48 +0600

    handling different type of errors with ()

--- exceptions/exception.py
+++ exceptions/exception.py
@@ -1,10 +1,10 @@
 # example when user enters not correct input
 try:
     age = int(input("Age: "))
-except ValueError as ex: # the ex part is considered as very technical, and may not be included, this is for illustation purpouses only
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError): # the ex part is considered as very technical, and may not be included, this is for illustation purpouses only
+    # if in paranthese we put another type of error like ZeroDivisionError in this case, in the case of age given 0 
+    # the program will authomatically give similar error message and will not crash
     print("You did not enter a valid age.")
-    print(ex)
-    print(type(ex))
 else: # this part of code is executed if the except part was not executed
     print("No exceptions were encountered.")
-print("Going on smoothly") # this message shows that the code is still operating and have not crashed
