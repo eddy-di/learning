4f66f6f0df461a9fe3fb1709b451ec1fd90463d9
Eddy <eddy.di.minur@gmail.com>
2023-02-15 11:50:19 +0600

    minor changes

--- classes/class&instanceattributes.py
+++ classes/class&instanceattributes.py
@@ -4,9 +4,10 @@ class Human:
         self.height = int(height)
         self.hair_color = hair_color
         self.eye_color = eye_color
+    def who(self):
+        print(f"This human has {self.height} cm height, {self.hair_color} hairs, and {self.eye_color} eyes.")
 
 
 human1 = Human(166, "dark", "brown")
-print(human1.eye_color)
+human1.who()
 human2 = Human(178, "blonde", "blue")
-print(human2.height)
