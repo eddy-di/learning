2f75dd84f440ff57966b1aabfabc50d8b988463b
Eddy <eddy.di.minur@gmail.com>
2023-03-26 16:41:58 +0600

    other ways of editing property and making it

--- classes/propertiesclass.py
+++ classes/propertiesclass.py
@@ -1,15 +1,18 @@
 class Product:
     def __init__(self, price):
-        self.set_price(price)
+        self.price = price
 # There are times when users may input negative values to product prices. Python accepts it but we need to create a logic of making it not possible
-    def get_price(self): # This method takes an input from a user
+# third option is to create properties in a class and make it work just like the line with price = property(get_price, set_price)
+    @property
+    def price(self): # This method takes an input from a user
         return self.__price
-    def set_price(self, value):
+    @price.setter
+    def price(self, value):
         if value < 0: # This part checks if the price is negative, and if it is like that, will give an error to a user
             raise ValueError("Price cannot be negative.")
         self.__price = value
-# one of the ways of implementing a code. But this way is considered as a bit noisy and not pythonic
-    price = property(get_price, set_price) # this is considered as more pythonic in a sense
+
 
 product = Product(100)
+product.price = -1
 print(product.price)
