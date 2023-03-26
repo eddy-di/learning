4b97ec2169724341732ce1f6bb0bdd521043061c
Eddy <eddy.di.minur@gmail.com>
2023-03-26 16:01:41 +0600

    properties in classes video 11

---
+++ classes/propertiesclass.py
@@ -0,0 +1,13 @@
+class Product:
+    def __init__(self, price):
+        self.set_price(price)
+# There are times when users may input negative values to product prices. Python accepts it but we need to create a logic of making it not possible
+    def get_price(self): # This method takes an input from a user
+        return self.__price
+    def set_price(self, value):
+        if value < 0: # This part checks if the price is negative, and if it is like that, will give an error to a user
+            raise ValueError("Price cannot be negative.")
+        self.__price = value
+# one of the ways of implementing a code. But this way is considered as a bit noisy and not pythonic
+product = Product(-100)
+print(product.__price)
