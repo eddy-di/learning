b5a00b00ecbca2b759a335c8c6f2881c43c9b79f
Eddy <eddy.di.minur@gmail.com>
2023-04-02 13:21:04 +0600

    commets based on video 20

--- classes/polymorphism.py
+++ classes/polymorphism.py
@@ -18,6 +18,9 @@ def draw(control): # this is an instance of polymorphism, it allows to write cle
     # good for readability and clean code kinda 
     control.draw()
 
+# Python is dynamic language and it supports a duck typing principle. In this example you don't need to put abstract classes and methods, and polymorphic behaviour will still be working. 
+# But it is better to leave the abstract classes and methods, because it is a good practice of making sure that every other classes that is related to UIControl class has this abstract method.
+
 ddl = DropDownList()
 draw(ddl)
 txt = TextBox()
