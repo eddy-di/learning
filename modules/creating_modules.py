21f850736c1fefa95f99ccb35140da8108dc4b1c
Eddy <eddy.di.minur@gmail.com>
2023-04-02 15:44:47 +0600

    video 1 modules

---
+++ modules/creating_modules.py
@@ -0,0 +1,8 @@
+from mod1 import calc_shipping, calc_tax
+import mod1
+# this is an example of using modules (mod1.py) in python and using them, with the ability to utilize created functions, classes or variables
+mod1.calc_shipping()
+mod1.calc_tax()
+
+calc_tax()
+calc_shipping()
