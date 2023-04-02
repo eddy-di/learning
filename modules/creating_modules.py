5e9d605e3da01182867c87d00252f132b7366e47
Eddy <eddy.di.minur@gmail.com>
2023-04-02 16:09:11 +0600

    video3

--- modules/creating_modules.py
+++ modules/creating_modules.py
@@ -1,10 +1,11 @@
 from mod1 import calc_shipping, calc_tax
 import mod1
+import sys
 # this is an example of using modules (mod1.py) in python and using them, with the ability to utilize created functions, classes or variables
 mod1.calc_shipping()
 mod1.calc_tax()
 
 # after opening of the file in terminal the __pycache__ appers, it is necessary to access faster to the imported modules
-
+print(sys.path) # this shows where python will be looking for other modules when they are imported to project
 calc_tax()
 calc_shipping()
