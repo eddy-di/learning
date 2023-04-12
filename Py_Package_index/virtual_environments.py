098457b9fb090c59b6cac081d8f7b706ce9d1bce
Eddy <eddy.di.minur@gmail.com>
2023-04-12 14:31:04 +0600

    video 4 and partly 5

--- Py_Package_index/virtual_environments.py
+++ Py_Package_index/virtual_environments.py
@@ -1,2 +1,12 @@
 # all the necessary steps are made in the 3rd video of the folder. 
 # The main code to activate the environment has to be made in cmd part for win
+
+# furter are the notes from video 4
+# To work with virtual environments need to first install pipenv with command "pip install pipenv"
+# then with the abilities of pipenv, can call the virtual environment with command "pipenv shell"
+# shell is a virtual env that will be useful while working on several projects
+
+# further are the notes from video 5
+#
+#
+#
