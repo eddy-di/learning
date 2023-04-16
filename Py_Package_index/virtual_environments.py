38f1f16a83812e58555883a6f1aa4d104e71715c
Eddy <eddy.di.minur@gmail.com>
2023-04-16 16:43:40 +0600

    comments and changes

--- Py_Package_index/virtual_environments.py
+++ Py_Package_index/virtual_environments.py
@@ -7,6 +7,12 @@
 # shell is a virtual env that will be useful while working on several projects
 
 # further are the notes from video 5
-#
-#
-#
+# The main idea of a video is to make by default the virtual env to be running or setting such thing
+# For now 12/04/2023 I have some problems with working it out in vscode for windows
+
+# video 6 
+# Main idea of the lesson is to show that the pipenv can take/install dependencies from either pipfile or pipfile.lock
+# commands that are shown in video are applicable to the linux OS and not windows 
+# need to install Linux OS
+
+print("something")
